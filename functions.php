1341f7f53786dc97327cb05407474dd618f78ad6
Justin Klemm <jwklemm@gmail.com>
2013-06-25 01:30:44 -0400

    Add some initial JS back from _s base theme

--- functions.php
+++ functions.php
@@ -106,7 +106,6 @@ function crisp_persona_scripts() {
 	wp_enqueue_script( 'crisp-persona-main', get_template_directory_uri() . '/js/main.js', array( 'jquery' ), '20130601', false );
 	wp_enqueue_script( 'crisp-persona-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130601', true );
 	
-	/*
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
@@ -114,7 +113,6 @@ function crisp_persona_scripts() {
 	if ( is_singular() && wp_attachment_is_image() ) {
 		wp_enqueue_script( 'crisp-persona-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20130601' );
 	}
-	*/
 }
 add_action( 'wp_enqueue_scripts', 'crisp_persona_scripts' );
 
