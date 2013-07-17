2233387ce48b33e5712e9e84a615d33499b2c6ba
Justin Klemm <jwklemm@gmail.com>
2013-07-17 14:34:09 -0400

    Infinite scroll fix

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -26,7 +26,7 @@ function crisp_persona_content_nav( $nav_id ) {
 	// Check for infinite scrolling and skip nav if it's enabled
 	$infinite_scroll = false;
 	$jetpack = get_option('jetpack_active_modules');
-	if(is_array($jetpack) && in_array('infinite-scroll', $jetpack)) $infinite_scroll = true;
+	if( get_option('infinite_scroll') || (is_array($jetpack) && in_array('infinite-scroll', $jetpack)) ) $infinite_scroll = true;
 
 	// Don't print empty markup in archives if there's only one page.
 	if ( ($wp_query->max_num_pages < 2 || $infinite_scroll) && ( is_home() || is_archive() || is_search() ) )
